5bcf1a613209f9df3ffe27f88fac28ba4e19c0fe
tian <valaxy.tian@gmail.com>
2017-01-29 13:10:58 +0800

    Update gallina.v

--- coq/gallina.v
+++ coq/gallina.v
@@ -4,3 +4,9 @@ Require Import Arith. (* 加载Arith库 *)
 Open Scope Z_scope. (* 开启辖域 *)
 Locate "_ * _". (* 识别给定符号是哪种解释 *)
 let square := diff * diff in square * square (* 局部绑定 *)
+
+(* 证明策略 *)
+intros.
+apply.
+assumption.
+exact.
